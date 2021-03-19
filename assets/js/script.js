bc242f10c2c8faabd266f25ba0f63381c8c6c06c
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-19 16:17:18 +0100

    async function doesn't work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,112 +1,124 @@
-let questions = [{
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "What does CPU stand for?",
-        correct_answer: "Central Processing Unit",
-        incorrect_answers: [
-            "Central Process Unit",
-            "Computer Personal Unit",
-            "Central Processor Unit",
-        ],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
-        correct_answer: "Final",
-        incorrect_answers: ["Static", "Private", "Public"],
-    },
-    {
-        category: "Science: Computers",
-        type: "boolean",
-        difficulty: "easy",
-        question: "The logo for Snapchat is a Bell.",
-        correct_answer: "False",
-        incorrect_answers: ["True"],
-    },
-    {
-        category: "Science: Computers",
-        type: "boolean",
-        difficulty: "easy",
-        question: "Pointers were not used in the original C programming language; they were added later on in C++.",
-        correct_answer: "False",
-        incorrect_answers: ["True"],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "What is the most preferred image format used for logos in the Wikimedia database?",
-        correct_answer: ".svg",
-        incorrect_answers: [".png", ".jpeg", ".gif"],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "In web design, what does CSS stand for?",
-        correct_answer: "Cascading Style Sheet",
-        incorrect_answers: [
-            "Counter Strike: Source",
-            "Corrective Style Sheet",
-            "Computer Style Sheet",
-        ],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "What is the code name for the mobile operating system Android 7.0?",
-        correct_answer: "Nougat",
-        incorrect_answers: [
-            "Ice Cream Sandwich",
-            "Jelly Bean",
-            "Marshmallow",
-        ],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "On Twitter, what is the character limit for a Tweet?",
-        correct_answer: "140",
-        incorrect_answers: ["120", "160", "100"],
-    },
-    {
-        category: "Science: Computers",
-        type: "boolean",
-        difficulty: "easy",
-        question: "Linux was first created as an alternative to Windows XP.",
-        correct_answer: "False",
-        incorrect_answers: ["True"],
-    },
-    {
-        category: "Science: Computers",
-        type: "multiple",
-        difficulty: "easy",
-        question: "Which programming language shares its name with an island in Indonesia?",
-        correct_answer: "Java",
-        incorrect_answers: ["Python", "C", "Jakarta"],
-    },
-];
+// let questions = [{
+//         category: "Science: Computers",
+//         type: "multiple",
+//         difficulty: "easy",
+//         question: "What does CPU stand for?",
+//         correct_answer: "Central Processing Unit",
+//         incorrect_answers: [
+//             "Central Process Unit",
+//             "Computer Personal Unit",
+//             "Central Processor Unit",
+//         ],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "multiple",
+//         difficulty: "easy",
+//         question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
+//         correct_answer: "Final",
+//         incorrect_answers: ["Static", "Private", "Public"],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "boolean",
+//         difficulty: "easy",
+//         question: "The logo for Snapchat is a Bell.",
+//         correct_answer: "False",
+//         incorrect_answers: ["True"],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "boolean",
+//         difficulty: "easy",
+//         question: "Pointers were not used in the original C programming language; they were added later on in C++.",
+//         correct_answer: "False",
+//         incorrect_answers: ["True"],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "multiple",
+//         difficulty: "easy",
+//         question: "What is the most preferred image format used for logos in the Wikimedia database?",
+//         correct_answer: ".svg",
+//         incorrect_answers: [".png", ".jpeg", ".gif"],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "multiple",
+//         difficulty: "easy",
+//         question: "In web design, what does CSS stand for?",
+//         correct_answer: "Cascading Style Sheet",
+//         incorrect_answers: [
+//             "Counter Strike: Source",
+//             "Corrective Style Sheet",
+//             "Computer Style Sheet",
+//         ],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "multiple",
+//         difficulty: "easy",
+//         question: "What is the code name for the mobile operating system Android 7.0?",
+//         correct_answer: "Nougat",
+//         incorrect_answers: [
+//             "Ice Cream Sandwich",
+//             "Jelly Bean",
+//             "Marshmallow",
+//         ],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "multiple",
+//         difficulty: "easy",
+//         question: "On Twitter, what is the character limit for a Tweet?",
+//         correct_answer: "140",
+//         incorrect_answers: ["120", "160", "100"],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "boolean",
+//         difficulty: "easy",
+//         question: "Linux was first created as an alternative to Windows XP.",
+//         correct_answer: "False",
+//         incorrect_answers: ["True"],
+//     },
+//     {
+//         category: "Science: Computers",
+//         type: "multiple",
+//         difficulty: "easy",
+//         question: "Which programming language shares its name with an island in Indonesia?",
+//         correct_answer: "Java",
+//         incorrect_answers: ["Python", "C", "Jakarta"],
+//     },
+// ];
 
 let score = 0
+let questions = undefined
 let userAnswers = []
-let count = 0 //number of questions
+let count = 10 //number of questions
 let question = document.querySelector("#currentQuestion")
 let options = document.querySelector("#options")
 
 
 
-const displayQuestion = (count) => {
-    question.innerHTML = questions[count].question
-    generateOptionButtons(count)
+const grabData = async() => {
+    let category = parseInt(document.querySelector("#category").value)
+    let difficulty = document.querySelector("#difficulty").value
+    let count = parseInt(document.querySelector("#numOfQuestions").value)
+        //fetch data
+    let response = await fetch(`https://opentdb.com/api.php?amount=${count}&category=${category}&difficulty=${difficulty}`)
+    let data = await response.json()
+    console.log('data:', data)
 
+
+    //anotherFunction()
+    //generateAndFilQuestionAnswerElements(data)
 }
 
-const generateOptionButtons = () => {
+const generateAndFilQuestionAnswerElements = function(data) {
+    questions = data["results"]
+    console.log('questions:', questions)
+    question.innerHTML = questions[count].question
     let answers = questions[count].incorrect_answers
     answers.push(questions[count].correct_answer)
 
@@ -119,6 +131,7 @@ const generateOptionButtons = () => {
     }
 
 }
+
 
 const validateAnswer = (event, i) => {
     if (event.target.innerText === questions[0].correct_answer) {
@@ -141,16 +154,15 @@ const validateAnswer = (event, i) => {
 }
 
 
-
 window.onload = function() {
-    displayQuestion(count)
-        //IF YOU ARE DISPLAYING ALL THE QUESTIONS TOGETHER:
-        //HINT: for each question, create a container with the "question"
-        //create a radio button https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio with, as option the both the correct answer and the incorrect answers
-        //when EVERY question has an answer (google for how to get a value from a radio button with JS)
-        //IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
-        //Display first question with a title + radio button
-        //when the user select the answer, pick the next question and remove this from the page after added in a varible the users' choice.
+
+    //IF YOU ARE DISPLAYING ALL THE QUESTIONS TOGETHER:
+    //HINT: for each question, create a container with the "question"
+    //create a radio button https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio with, as option the both the correct answer and the incorrect answers
+    //when EVERY question has an answer (google for how to get a value from a radio button with JS)
+    //IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
+    //Display first question with a title + radio button
+    //when the user select the answer, pick the next question and remove this from the page after added in a varible the users' choice.
 };
 
 //HOW TO calculate the result
---
+++ assets/js/test.js
@@ -0,0 +1,19 @@
+let category = 18
+let difficulty = "easy"
+    // let count = 10
+let result = ""
+
+
+
+const grabData1 = async() => {
+    let response = await fetch(`https://opentdb.com/api.php?amount=${count}&category=${category}&difficulty=${difficulty}`)
+    let data = await response.json()
+    console.log('data:', data["results"])
+}
+
+
+
+//
+const startQuiz = () => {
+    grabData1()
+}
