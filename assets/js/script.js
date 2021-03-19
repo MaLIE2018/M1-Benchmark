689e7c0bf152a13efb14fc11e2b4c486e0f263a8
Maximilian Liebsch <43824628+MaLIE2018@users.noreply.github.com>
2021-03-19 17:13:11 +0100

    Resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,3 @@
-// let questions = [{
-//         category: "Science: Computers",
-//         type: "multiple",
-//         difficulty: "easy",
-//         question: "What does CPU stand for?",
-//         correct_answer: "Central Processing Unit",
-//         incorrect_answers: [
-//             "Central Process Unit",
-//             "Computer Personal Unit",
-//             "Central Processor Unit",
-//         ],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "multiple",
-//         difficulty: "easy",
-//         question: "In the programming language Java, which of these keywords would you put on a variable to make sure it doesn&#039;t get modified?",
-//         correct_answer: "Final",
-//         incorrect_answers: ["Static", "Private", "Public"],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "boolean",
-//         difficulty: "easy",
-//         question: "The logo for Snapchat is a Bell.",
-//         correct_answer: "False",
-//         incorrect_answers: ["True"],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "boolean",
-//         difficulty: "easy",
-//         question: "Pointers were not used in the original C programming language; they were added later on in C++.",
-//         correct_answer: "False",
-//         incorrect_answers: ["True"],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "multiple",
-//         difficulty: "easy",
-//         question: "What is the most preferred image format used for logos in the Wikimedia database?",
-//         correct_answer: ".svg",
-//         incorrect_answers: [".png", ".jpeg", ".gif"],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "multiple",
-//         difficulty: "easy",
-//         question: "In web design, what does CSS stand for?",
-//         correct_answer: "Cascading Style Sheet",
-//         incorrect_answers: [
-//             "Counter Strike: Source",
-//             "Corrective Style Sheet",
-//             "Computer Style Sheet",
-//         ],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "multiple",
-//         difficulty: "easy",
-//         question: "What is the code name for the mobile operating system Android 7.0?",
-//         correct_answer: "Nougat",
-//         incorrect_answers: [
-//             "Ice Cream Sandwich",
-//             "Jelly Bean",
-//             "Marshmallow",
-//         ],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "multiple",
-//         difficulty: "easy",
-//         question: "On Twitter, what is the character limit for a Tweet?",
-//         correct_answer: "140",
-//         incorrect_answers: ["120", "160", "100"],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "boolean",
-//         difficulty: "easy",
-//         question: "Linux was first created as an alternative to Windows XP.",
-//         correct_answer: "False",
-//         incorrect_answers: ["True"],
-//     },
-//     {
-//         category: "Science: Computers",
-//         type: "multiple",
-//         difficulty: "easy",
-//         question: "Which programming language shares its name with an island in Indonesia?",
-//         correct_answer: "Java",
-//         incorrect_answers: ["Python", "C", "Jakarta"],
-//     },
-// ];
-
 let score = 0
 let questions = undefined
 let userAnswers = []
@@ -104,6 +10,8 @@ let options = document.querySelector("#options")
 
 const grabData = async(event) => {
     event.preventDefault()
+    count = 0
+    options.innerHTML = ""
     let category = parseInt(document.querySelector("#category").value)
     let difficulty = document.querySelector("#difficulty").value
     let userChoice = parseInt(document.querySelector("#numOfQuestions").value)
@@ -115,8 +23,6 @@ const grabData = async(event) => {
 }
 
 const generateAndFilQuestionAnswerElements = function() {
-
-    console.log('questions:', questions)
     question.innerHTML = questions[count].question
     let answers = questions[count].incorrect_answers
     answers.push(questions[count].correct_answer)
@@ -132,8 +38,8 @@ const generateAndFilQuestionAnswerElements = function() {
 }
 
 
-const validateAnswer = (event, i) => {
-    if (event.target.innerText === questions[0].correct_answer) {
+const validateAnswer = (event) => {
+    if (event.target.innerText === questions[count].correct_answer) {
         score += 1
         event.target.style.backgroundColor = "green"
     } else {
@@ -152,20 +58,3 @@ const validateAnswer = (event, i) => {
     count++
 
 }
-
-
-window.onload = function() {
-
-    //IF YOU ARE DISPLAYING ALL THE QUESTIONS TOGETHER:
-    //HINT: for each question, create a container with the "question"
-    //create a radio button https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/radio with, as option the both the correct answer and the incorrect answers
-    //when EVERY question has an answer (google for how to get a value from a radio button with JS)
-    //IF YOU ARE DISPLAYING ONE QUESTION AT A TIME
-    //Display first question with a title + radio button
-    //when the user select the answer, pick the next question and remove this from the page after added in a varible the users' choice.
-};
-
-//HOW TO calculate the result
-//You can do it in 2 ways:
-//If you are presenting all questions together, just take all the radio buttons and check if the selected answer === correct_answer
-//If you are presenting one question at a time, just add one point or not to the user score if the selected answer === correct_answer
